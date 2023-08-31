ef1c959446b8119a812ea6a1ce259afef25c4cc8
Dmitry Basov <dom.it.ru@yandex.ru>
2023-08-31 20:28:10 +0300

    Add homework_06

---
+++ HomeWork_06/src/hw_06.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `Doctor` DROP FOREIGN KEY `Doctor_fk0`;
+
+ALTER TABLE `Phones` DROP FOREIGN KEY `Phones_fk0`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk0`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk1`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk2`;
+
+ALTER TABLE `Patient` DROP FOREIGN KEY `Patient_fk0`;
+
+ALTER TABLE `Patient` DROP FOREIGN KEY `Patient_fk1`;
+
+ALTER TABLE `Patient` DROP FOREIGN KEY `Patient_fk2`;
+
+ALTER TABLE `Diagnosis` DROP FOREIGN KEY `Diagnosis_fk0`;
+
+ALTER TABLE `Survey_results` DROP FOREIGN KEY `Survey_results_fk0`;
+
